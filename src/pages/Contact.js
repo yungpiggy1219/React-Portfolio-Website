5821243ddcbd99a5af0dc4ec64720b05fd6dd433
yungpiggy1219 <yungchc1@uci.edu>
2022-10-03 23:03:59 +0800

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,36 +1,26 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { Icon } from '@iconify/react';
 
 export default function Contact() {
     return (
         <Container className=''>
             <Row>
-                <h1>/about</h1>
+                <h1>/contact</h1>
             </Row>
 
             <Row>
-                <p>Hi, my name is Yung-Chu Chuang. I’m a UX engineer with design,
-                    research, and software engineer skills. My background in Informatics
-                    with specialization in Human-Computer Interaction and Art allows me
-                    to understand problems from both a technology and creative perspective.
-                    My mission is to end user’s misery by bringing the best experience to them.
-                </p>
+                <p><Icon icon="akar-icons:linkedin-box-fill"/>Yung-Chu Chuang</p>
             </Row>
 
             <Row>
-                <a>resume</a>
+                <p><Icon icon="ic:round-email" />yungchu1219@gmail.com</p>
+
             </Row>
 
             <Row>
-                <h1>/skill</h1>
+                <p><Icon icon="ant-design:phone-filled" />(773) 231-2717</p>
             </Row>
 
-            <Row>
-                <p>
-                    HTML & CSS / JavaScript / Java / Python / C++ / Data Analysis /
-                    UX Design / Prototyping / Wireframing / Figma /
-                    Human-Computer Interaction/ User Experience Research
-                </p>
-            </Row>
         </Container>
     )
 }
