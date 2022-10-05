977495a41ffefe7422be4b4b3d7eb27217793ce6
yungpiggy1219 <yungchc1@uci.edu>
2022-10-05 23:39:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AnimatedCursor from "react-animated-cursor"
 
 import { Row, Col } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
@@ -13,19 +16,27 @@ import Home from "./pages/Home";
 import Work from "./pages/Work";
 
 function App() {
+
   return (
-    <Router className="App">
+    <Router>
+      <AnimatedCursor
+        color="255,0,122"
+      />
       <Row>
-        <Col md={2}>
-          <Nav className="flex-column">
-            <Nav.Link as={ NavLink } to="/" end>home</Nav.Link>
-            <Nav.Link as={ NavLink } to="/about">about</Nav.Link>
-            <Nav.Link as={ NavLink } to="/work">work</Nav.Link>
-            <Nav.Link as={ NavLink } to="/contact">contact</Nav.Link>
-          </Nav>
+        <Col md={2} className='navBar'>
+          <Row>
+
+            <Nav className="flex-column">
+            <Navbar.Brand as={NavLink} to="/" className='myTitle top-0'>Yung-Chu Chuang</Navbar.Brand>
+              <Nav.Link as={NavLink} to="/" end>home <span className='indexText'>0</span></Nav.Link>
+              <Nav.Link as={NavLink} to="/about">about <span className='indexText'>1</span></Nav.Link>
+              <Nav.Link as={NavLink} to="/work">work <span className='indexText'>2</span></Nav.Link>
+              <Nav.Link as={NavLink} to="/contact">contact <span className='indexText'>3</span></Nav.Link>
+            </Nav>
+          </Row>
         </Col>
 
-        <Col>
+        <Col className='mainPage'>
           <Routes>
             <Route path='/' element={< Home />}></Route>
             <Route path='/about' element={< About />}></Route>
@@ -36,6 +47,5 @@ function App() {
       </Row>
     </Router>
   );
-
 }
 export default App;
