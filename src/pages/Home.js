13f38e2ad09cd0b50a58ec7b0e82ef953ed2fe7b
yungpiggy1219 <43735672+yungpiggy1219@users.noreply.github.com>
2023-12-05 23:23:50 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,13 @@ import { Row, Col } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 import monsterdex from "../images/monsterdex-banner.png";
 import likeex from "../images/likee-banner.png";
 import halfbyte from "../images/halfbyte-banner.png";
@@ -19,7 +26,7 @@ export default function Home() {
                     <div className='typing'> <div className='fixWord'>and I am a</div>
                         <Typewriter
                             options={{
-                                strings: ['designer.', ' gamer.', ' researcher.', ' developer.'],
+                                strings: ['produdct designer.', ' ux researcher.', ' ui developer.', ' gamer.'],
                                 autoStart: true,
                                 loop: true,
                                 delay: 60,
@@ -31,7 +38,26 @@ export default function Home() {
                     </div>
                 </h1>
             </Row>
-            <h4>my recent work</h4>
+            <h4>my works</h4>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 180 }}
+                    image={monsterdex}
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" className='muih5'>
+                        UX/UI Design
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        MonsterDex is a Pokédex app for Pokémon trainers! With detailed information on over 1200 Pokémons, 950+ items, 900+ moves, and 350+ abilities.
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" href='#/work/monsterdex'>Learn More</Button>
+                    <Button size="small" href='#/work/monsterdex'>App Store</Button>
+                </CardActions>
+            </Card>
             <Row className="galleryP recentWork">
                 <div class="media">
                     <div class="layer">
